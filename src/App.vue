b102f2e32cc828be17a24c04232057da53f33c13
Gene H <gshuh.mail@gmail.com>
2020-04-24 12:40:04 -0400

    Working single click version

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app" class="text-center">
+    <h1>Conway's Game of Life</h1>
     <Controls @createGrid="createGrid" @calcNext="calcNext" />
-    <div class="text-center">Generation {{genNum}}</div>
+    <div class="text-center">Generation {{ genNum }}</div>
     <div id="display-grid" class="mx-5">
       <table class="table table-bordered" :key="genNum">
         <tr v-for="(row, rowIndex) in grid" :key="rowIndex">
@@ -11,9 +12,17 @@
             v-for="(col, colIndex) in row"
             :key="colIndex"
           >
-            <div>{{grid[rowIndex][colIndex]}}</div>
-            <font-awesome-icon v-if="grid[rowIndex][colIndex]!=0" icon="square" color="black"></font-awesome-icon>
-            <font-awesome-icon v-else icon="square" color="white"></font-awesome-icon>
+            <div>{{ grid[rowIndex][colIndex] }}</div>
+            <font-awesome-icon
+              v-if="grid[rowIndex][colIndex] != 0"
+              icon="square"
+              color="black"
+            ></font-awesome-icon>
+            <font-awesome-icon
+              v-else
+              icon="square"
+              color="white"
+            ></font-awesome-icon>
           </td>
         </tr>
       </table>
@@ -50,26 +59,35 @@ export default {
           if (grid[row].length > 1) {
             // add X neighbor count if at least 2 cols
             nbTotal += nbCountX(row, col);
+            //console.log("Horizontal neighbor count is " + nbTotal);
           }
           if (grid.length > 1) {
             // if more than 1 row, add either below or above or both
             if (row == 0) {
               nbTotal += nbCountBelow(row, col);
-              console.log("did a count below");
+              console.log("did a count below, nbTotal is " + nbTotal);
             } else if (row == grid.length - 1) {
               nbTotal += nbCountAbove(row, col);
-              console.log("did a count above");
+              console.log("did a count above, nbTotal is " + nbTotal);
             } else {
               console.log("did both below and above");
               nbTotal += nbCountBelow(row, col) + nbCountAbove(row, col);
             }
           }
-          console.log("nbTotal after is " + nbTotal);
-          //nextGenGrid[row][col] = nbTotal;
+          console.log("nbTotal is " + nbTotal);
+          this.nextGen[row][col] = isDeadOrAlive(row, col, nbTotal);
         }
       }
-      //this.grid = nextGenGrid;
+      this.grid = this.nextGen;
       this.genNum++;
+
+      function isDeadOrAlive(row, col, neighborCount) {
+        let result = 0;
+        if (neighborCount == 3 || (grid[row][col] > 0 && neighborCount == 2)) {
+          result = 1;
+        }
+        return result;
+      }
       function nbCountX(row, col) {
         // add horizontal neighbors
         let count = 0;
@@ -88,32 +106,37 @@ export default {
         // add vertical neighbors if top row
         let count = 0;
         count += grid[row + 1][col]; //add lower neighbor directly underneath
-        if (col == 0) {
-          // add lower right neighbor if left corner
-          count += grid[row + 1][col + 1];
-        } else if (grid[0].length - 1 == col) {
-          // add lower left neighbor if right corner
-          count += grid[row + 1][col - 1];
-        } else if (grid[0].length > 2) {
-          // add both lower diagonal corners if not corner
-          count += grid[row + 1][col - 1] + grid[row + 1][col + 1];
+        //console.log("direct below is " + count);
+        if (grid[row].length > 1) {
+          if (col == 0) {
+            // add lower right neighbor if left corner
+            count += grid[row + 1][col + 1];
+          } else if (grid[0].length - 1 == col) {
+            // add lower left neighbor if right corner
+            count += grid[row + 1][col - 1];
+          } else if (grid[0].length > 2) {
+            // add both lower diagonal corners if not corner
+            count += grid[row + 1][col - 1] + grid[row + 1][col + 1];
+          }
         }
-        console.log("count is " + count);
         return count;
       }
       function nbCountAbove(row, col) {
         // add vertical neighbors if bottom row
         let count = 0;
         count += grid[row - 1][col]; //add upper neighbor directly above
-        if (col == 0) {
-          // add upper right neighbor if left corner
-          count += grid[row - 1][col + 1];
-        } else if (grid[0].length - 1 == col) {
-          // add upper left neighbor if right corner
-          count += grid[row - 1][col - 1];
-        } else if (grid[0].length > 2) {
-          // add both upper diagonal corners if not corner
-          count += grid[row - 1][col - 1] + grid[row - 1][col + 1];
+        //console.log("direct above is " + count);
+        if (grid[row].length > 1) {
+          if (col == 0) {
+            // add upper right neighbor if left corner
+            count += grid[row - 1][col + 1];
+          } else if (grid[0].length - 1 == col) {
+            // add upper left neighbor if right corner
+            count += grid[row - 1][col - 1];
+          } else if (grid[0].length > 2) {
+            // add both upper diagonal corners if not corner
+            count += grid[row - 1][col - 1] + grid[row - 1][col + 1];
+          }
         }
         return count;
       }
